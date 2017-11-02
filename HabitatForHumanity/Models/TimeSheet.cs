12634d0267d58b09953b52431f62b625b39095d6
Unknown <10bjohnson2@gmail.com>
2017-11-02 00:13:07 -0600

    added method for finding timesheet by primary key

--- HabitatForHumanity/Models/TimeSheet.cs
+++ HabitatForHumanity/Models/TimeSheet.cs
@@ -16,5 +16,21 @@ namespace HabitatForHumanity.Models
         public int project_Id { get; set; }
         public DateTime clockInTime { get; set; }
         public DateTime clockOutTime { get; set; }
+
+        #region Database Access Methods
+        /// <summary>
+        /// Gets the record in the timesheet table by it's natural key: user_id+project_id+clockInTime.
+        /// </summary>
+        /// <param name="userId">Id of the user</param>
+        /// <param name="projectId">Id of the project</param>
+        /// <param name="clockInTime">MM/DD/YYYY</param>
+        /// <returns>Timesheet Object</returns>
+        public TimeSheet GetTimeSheetByPrimaryKey(int userId, int projectId, string clockInTime)
+        {
+            VolunteerDbContext db = new VolunteerDbContext();
+            DateTime cit = DateTime.Parse(clockInTime);
+            return db.timeSheets.Where(x => x.user_Id == userId && x.project_Id == projectId && x.clockInTime.Equals(cit)).Single();
+        }
+        #endregion
     }
 }
