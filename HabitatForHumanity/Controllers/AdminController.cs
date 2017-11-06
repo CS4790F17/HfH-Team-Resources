d24cf3d810e292c74bf4aa29c8f9e84ca2da4430
Steve Julien <stevenjulien@mail.weber.edu>
2017-11-05 21:39:19 -0700

    generic chart loaded from controller

--- HabitatForHumanity/Controllers/AdminController.cs
+++ HabitatForHumanity/Controllers/AdminController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DotNet.Highcharts;
+using DotNet.Highcharts.Enums;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+using System.Drawing;
+using HabitatForHumanity.ViewModels;
 
 namespace HabitatForHumanity.Controllers
 {
@@ -17,6 +23,91 @@ namespace HabitatForHumanity.Controllers
         public ActionResult Dashboard()
         {
             return View();
+        }
+
+        public ActionResult Chart()
+        {
+            ChartVM chartVM = new ChartVM();
+            //String[] xCategories = new String[] { "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012" };
+            Highcharts columnChart = new Highcharts("columnchart");
+
+            columnChart.InitChart(new Chart()
+            {
+                Type = DotNet.Highcharts.Enums.ChartTypes.Column,
+                BackgroundColor = new BackColorOrGradient(System.Drawing.Color.AliceBlue),
+                Style = "fontWeight: 'bold', fontSize: '17px'",
+                BorderColor = System.Drawing.Color.LightBlue,
+                BorderRadius = 0,
+                BorderWidth = 2
+
+            });
+
+            columnChart.SetTitle(new Title()
+            {
+                //Text = "Volunteer Hours"
+                Text = chartVM._title
+            });
+
+            columnChart.SetSubtitle(new Subtitle()
+            {
+                //Text = "Subtitle here"
+                Text = chartVM._subtitle
+            });
+
+            columnChart.SetXAxis(new XAxis()
+            {
+                Type = AxisTypes.Category,
+                Title = new XAxisTitle() { Text = "X axis stuff", Style = "fontWeight: 'bold', fontSize: '17px'" },
+                Categories = chartVM._categories
+            });
+
+            columnChart.SetYAxis(new YAxis()
+            {
+                Title = new YAxisTitle()
+                {
+                    Text = "Hours",
+                    Style = "fontWeight: 'bold', fontSize: '17px'"
+                },
+                ShowFirstLabel = true,
+                ShowLastLabel = true,
+                Min = 0
+            });
+
+            columnChart.SetLegend(new Legend
+            {
+                Enabled = true,
+                BorderColor = System.Drawing.Color.CornflowerBlue,
+                BorderRadius = 6,
+                BackgroundColor = new BackColorOrGradient(ColorTranslator.FromHtml("#FFADD8E6"))
+            });
+
+
+            columnChart.SetSeries(new Series[]
+            //columnChart.SetSeries(new Series[] = chartVM._series;
+            {
+
+
+                new Series
+                {
+
+                    Name = chartVM._series[0]._name,
+                    Data = new Data(chartVM._series[0]._data)
+                },
+                new Series
+                {
+
+                    Name = "Re-Store",
+                    Data = new Data(new object[] { 312, 42, 928, 425, 660, 572, 604, 713, 115 })
+                },
+                new Series
+                {
+                    Name = "ABWK",
+                    Data = new Data(new object[] { 19, 895, 821, 1103, 1097, 1198, 600, 764, 524, })
+                }
+            }
+            );
+
+            return View(columnChart);
         }
     }
 }
