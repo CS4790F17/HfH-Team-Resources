7c9c636b3c75988e3e9585b1319d58151ade45d7
Blake <10bjohnson2@gmail.com>
2017-11-08 10:58:40 -0700

    added ReturnStatus model

---
+++ HabitatForHumanity/Models/ReturnStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HabitatForHumanity.Models
+{
+    public class ReturnStatus
+    {
+        public int errorCode { get; set; }
+        public object data { get; set; }
+    }
+}
