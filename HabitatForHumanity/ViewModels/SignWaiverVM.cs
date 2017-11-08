809ebe9732d332a292af866a2c23d95b4091f45e
Trevor Orgill <trevororgill@weber.edu>
2017-11-08 10:07:29 -0700

    Fixed merge conflics as well as added SignWaiver and VolunteerSignup

--- HabitatForHumanity/ViewModels/SignWaiverVM.cs
+++ HabitatForHumanity/ViewModels/SignWaiverVM.cs
@@ -2,33 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using HabitatForHumanity.Models;
 
 namespace HabitatForHumanity.ViewModels
 {
     public class SignWaiverVM
     {
-        // join stuff
-        public int waiverId { get; set; }
-        public int userId { get; set; }
-
-        //user stuff
-        public string email { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string streetAddress { get; set; }
-        public string city { get; set; }
-        public string zip { get; set; }
-        public string phone { get; set; }
-
-        // waiver stuff
-        public DateTime signDate { get; set; }
-        public string emContFirstName { get; set; }
-        public string emContLastName { get; set; }
-        public string relation { get; set; }
-        public string emContHomePhone { get; set; }
-        public string emContWorkPhone { get; set; }
-        public string signature { get; set; } // change me later to an image
-        public bool consent { get; set; }
-
+        public User user { get; set; }
+        public string signature { get; set; }
     }
 }
