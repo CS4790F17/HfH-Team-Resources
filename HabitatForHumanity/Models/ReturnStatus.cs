5457e21a031c7f8ee30677a3ad21d2ef93695e1e
Blake <10bjohnson2@gmail.com>
2017-11-08 17:40:37 -0700

    /ErrorHandling: added tryParseUser
    attempts to parse a user out of given returnstatus data property

--- HabitatForHumanity/Models/ReturnStatus.cs
+++ HabitatForHumanity/Models/ReturnStatus.cs
@@ -9,5 +9,41 @@ namespace HabitatForHumanity.Models
     {
         public int errorCode { get; set; }
         public object data { get; set; }
+
+
+        public enum ErrorCodes
+        {
+            All_CLEAR,
+            COULD_NOT_CONNECT_TO_DATABASE,
+        };
+
+
+        /// <summary>
+        /// Tries to safely parse a user out of a ReturnStatus object
+        /// </summary>
+        /// <param name="st">ReturnStatus object that potentially contains a user in it's data property.</param>
+        /// <param name="result">The object reference in which to store the parsed user.</param>
+        /// <returns>True if it was capable of parsing a User, false otherwise</returns>
+        public static bool tryParseUser(ReturnStatus st, out User result)
+        {
+
+            result = new User();
+            try
+            {
+                if (st.errorCode == 0 && st.data != null)
+                {
+                    result = (User)st.data;
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                //TODO: log e
+                //could not parse user
+                return false;
+
+            }
+        }
     }
 }
