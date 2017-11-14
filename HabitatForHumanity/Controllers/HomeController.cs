a236a54a521b111946cf62cdec6a4cc106d18c27
Blake <10bjohnson2@gmail.com>
2017-11-13 18:40:34 -0700

    removed unused method

--- HabitatForHumanity/Controllers/HomeController.cs
+++ HabitatForHumanity/Controllers/HomeController.cs
@@ -17,33 +17,7 @@ namespace HabitatForHumanity.Controllers
         {
             return View();
         }
-
-        public ActionResult Examples()
-        {
-            ToolKitExampleVM model = new ToolKitExampleVM();
-            model.odd.createDropDownList(Repository.GetAllOrganizations());
-            model.pdd.createDropDownList(Repository.GetAllProjects());
-
-            model.projectTest = Project.GetProjectByNameAndDate("test", "1/1/2081").name;
-            model.projectTest2 = Project.GetProjectByNameAndDate("test2", "1/1/2081").name;
-
-            //Repository.GetUserByName("test1", "LASTname");
-            var temp = Repository.GetUsersByName("", "lastname");
-
-            var test = Repository.GetAllTimeSheetsByProjectId(1);
-            Repository.GetAllTimeSheetsByVolunteer(1);
-            var timetest = Repository.AddTimeSheetHours(test);
-            var volunteerTest = Repository.GetAllTimeSheetsByVolunteer(6);
-            var orgtest = Repository.GetAllTimeSheetsByOrganizationId(1);
-
-            DateTime beginDate = DateTime.Parse("11/1/2017");
-            DateTime endDate = DateTime.Parse("11/3/2017");
-
-            var dateTest = Repository.GetAllTimeSheetsInDateRange(beginDate, endDate);
-
-            return View(model);
-        }
-
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
