1c408fa2fa9a04f723129bbe5f123164506db809
Unknown <10bjohnson2@gmail.com>
2017-11-20 21:17:27 -0700

    added docstring. create button now opens modal

--- HabitatForHumanity/ViewModels/ToolKitVM.cs
+++ HabitatForHumanity/ViewModels/ToolKitVM.cs
@@ -9,20 +9,35 @@ namespace HabitatForHumanity.ViewModels
 {
     public static class ToolKitVM
     {
-        public static IHtmlString Modal(string title)
+        /// <summary>
+        /// Creates a Modal with the given properties
+        /// </summary>
+        /// <param name="title">The title that the modal is to display.</param>
+        /// <param name="divId">The id of the Modal div.</param>
+        /// <param name="partialTarget">The innermost div that the partial page needs to target.</param>
+        /// <returns></returns>
+        public static IHtmlString Modal(string title, string divId, string partialTarget)
         {
-            string htmlString = String.Format("<!-- Modal --><div class=\"modal fade\" id=\"adminModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"adminModalLabel\"><div class=\"modal-dialog\" role=\"document\">" +
+            string htmlString = String.Format("<!-- Modal --><div class=\"modal fade\" id=\"{1}\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"{1}Label\"><div class=\"modal-dialog\" role=\"document\">" +
                 "<div class=\"modal-content\"><div class=\"modal-header\">" +
                 "<button type = \"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>" +
-                "<h4 class=\"modal-title\" id=\"adminModallLabel\">{0}</h4></div>" +
+                "<h4 class=\"modal-title\" id=\"{1}lLabel\">{0}</h4></div>" +
                 " <div class=\"modal-body\">" +
-                "<!--The Div to insert returned partial into--><div id = \"partialTarget\"></div></div></div></div></div>", title);
+                "<!--The Div to insert returned partial into--><div id = \"{2}\"></div></div></div></div></div>", title, divId, partialTarget);
             return new HtmlString(htmlString);
         }
 
-        public static IHtmlString ModalButton(int id)
+        /// <summary>
+        /// Creates a Button that can activate a modal.
+        /// </summary>
+        /// <param name="elementClass">The class of the button. Used for ajax targeting: $("[elementClass]").attr("dataId")</param>
+        /// <param name="targetModal">The modal that opens when the button is clicked</param>
+        /// <param name="innerHtml">Html inside of the button itself. Useful for using spans with glyphicons in them.</param>
+        /// <param name="dataId">The id of the data type used by the button. If my VolunteerId is 1, my dataId would be 1.</param>
+        /// <returns></returns>
+        public static IHtmlString ModalButton(string elementClass, string targetModal, string innerHtml, string dataId)
         {
-            string htmlString = String.Format("<button targetId={0} type=\"button\" class=\"btn editButton\" data-toggle=\"modal\" data-target=\"#adminModal\"><span class=\"glyphicon glyphicon-pencil\" aria- hidden=\"true\" title=\"Edit\"></span></button>", id);
+            string htmlString = String.Format("<button  dataId={3} type=\"button\" class=\"btn {0}\" data-toggle=\"modal\" data-target=\"#{1}\">{2}</button>", elementClass, targetModal, innerHtml, dataId);
             return new HtmlString(htmlString);
         }
     }
