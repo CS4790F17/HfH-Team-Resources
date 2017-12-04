c8a7a73604cca8b16f426e99302f50a6a4ff15b5
Steve Julien <stevenjulien@mail.weber.edu>
2017-12-03 21:46:06 -0700

    no punchOut timecards are colored in ManageVolunteer

--- HabitatForHumanity/Controllers/ReportsController.cs
+++ HabitatForHumanity/Controllers/ReportsController.cs
@@ -8,8 +8,8 @@ using System.Web.Mvc;
 
 namespace HabitatForHumanity.Controllers
 {
-    //[AdminFilter]
-    //[AuthorizationFilter]
+    [AdminFilter]
+    [AuthorizationFilter]
     public class ReportsController : Controller
     {
         // GET: Reports
@@ -20,21 +20,16 @@ namespace HabitatForHumanity.Controllers
 
         public ActionResult ProjectDemographics()//int period)
         {
-            //List<ProjDemogReportVM> report = new List<ProjDemogReportVM>();
             List<List<ProjDemogReportVM>> monthsReports = new List<List<ProjDemogReportVM>>();
             ReturnStatus rs = Repository.GetProjectDemographicsReport(Project.YEARLY);
             if (rs.errorCode == ReturnStatus.ALL_CLEAR)
             {
                 monthsReports = (List<List<ProjDemogReportVM>>)rs.data;
             }
-            //List<List<ProjDemogReportVM>> monthsReports = new List<List<ProjDemogReportVM>>();
-            //monthsReports.Add(report);
-            //monthsReports.Add(report);
             return View(monthsReports);
             //            public static int MONTH = 1;
             //public static int QUARTER = 3;
             //public static int YEARLY = 12;
-            //public static ReturnStatus GetProjectDemographicsReport(int period)
         }
     }
 }
