7783b54c23975c7e8d19bfeeea4a278145b8848f
Justin Cherry <justin.cherry@live.com>
2017-12-05 22:19:24 -0700

    try-catch project controller complete

--- HabitatForHumanity/Controllers/ProjectController.cs
+++ HabitatForHumanity/Controllers/ProjectController.cs
@@ -22,65 +22,74 @@ namespace HabitatForHumanity.Controllers
         // GET: Project
         public ActionResult Index()
         {
-            List<Project> temp = new List<Project>();
-            temp = (List<Project>)Repository.GetAllProjects().data;
-            List<User> userIds = new List<User>();
-            List<ProjectIndexVM> index = new List<ProjectIndexVM>();
-           
-
-            foreach (Project project in temp)
+            try
             {
-                ProjectIndexVM projectIndexVM = new ProjectIndexVM();
-                projectIndexVM._name = project.name;
-                projectIndexVM._Id = project.Id;
-                projectIndexVM._beginDate = project.beginDate;
-                projectIndexVM._hoursLogged = (double)Repository.getTotalHoursLoggedIntoProject(project.Id).data;
-                userIds = (List<User>)TimeSheet.GetUsersbyTimeSheetFilters(project.Id, 0).data;
-                projectIndexVM._numVolunteers = userIds.Count();
-                index.Add(projectIndexVM);
+                List<Project> temp = new List<Project>();
+                temp = (List<Project>)Repository.GetAllProjects().data;
+                List<User> userIds = new List<User>();
+                List<ProjectIndexVM> index = new List<ProjectIndexVM>();
+
+                foreach (Project project in temp)
+                {
+                    ProjectIndexVM projectIndexVM = new ProjectIndexVM();
+                    projectIndexVM._name = project.name;
+                    projectIndexVM._Id = project.Id;
+                    projectIndexVM._beginDate = project.beginDate;
+                    projectIndexVM._hoursLogged = (double)Repository.getTotalHoursLoggedIntoProject(project.Id).data;
+                    userIds = (List<User>)TimeSheet.GetUsersbyTimeSheetFilters(project.Id, 0).data;
+                    projectIndexVM._numVolunteers = userIds.Count();
+                    index.Add(projectIndexVM);
+                }
+
+                return View(index);
             }
-           
-            return View(index);
+            catch
+            {
+                return View("Error");
+            }
         }
 
         // GET: Project/Details/5
         public ActionResult Details(int? id)
         {
-            int projectId = 0;
-            projectId = (int)id;
-
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+                int projectId = 0;
+                projectId = (int)id;
 
-            Project project = new Project();
-            ProjectVM projectVM = new ProjectVM();
-            //TODO: add error checking
-            project = (Project)Repository.GetProjectById(projectId).data;
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
 
-            //include list of people who have logged hrs. 
-            //include total hours logged into this project
-            //maybe include list of everyone assigned to this project
-            projectVM._hoursLogged = (double)Repository.getTotalHoursLoggedIntoProject(project.Id).data;
-            projectVM._Id = project.Id;
-            projectVM._name = project.name;
-            projectVM._beginDate = project.beginDate;
-            projectVM._description = project.description;
-            if (project.status == 1)
-            {
-                projectVM._status = true;
-            }
-            else
-            {
-                projectVM._status = false;
-            }
+                Project project = new Project();
+                ProjectVM projectVM = new ProjectVM();        
+                project = (Project)Repository.GetProjectById(projectId).data;
 
-            if (projectVM == null)
-            {
-                return HttpNotFound();
+                projectVM._hoursLogged = (double)Repository.getTotalHoursLoggedIntoProject(project.Id).data;
+                projectVM._Id = project.Id;
+                projectVM._name = project.name;
+                projectVM._beginDate = project.beginDate;
+                projectVM._description = project.description;
+                if (project.status == 1)
+                {
+                    projectVM._status = true;
+                }
+                else
+                {
+                    projectVM._status = false;
+                }
+
+                if (projectVM == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(projectVM);
             }
-            return View(projectVM);
+            catch
+            {
+                return View("Error");
+            }
         }
 
         // GET: Project/Create
@@ -96,52 +105,66 @@ namespace HabitatForHumanity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,name,description,beginDate")] Project project)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.projects.Add(project);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                if (ModelState.IsValid)
+                {
+                    db.projects.Add(project);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
 
-            return View(project);
+                return View(project);
+            }
+            catch
+            {
+                return View("Error");
+            }
         }
 
         // GET: Project/Edit/5
         public ActionResult Edit(int? id)
         {
-            int projectId = 0;
-            projectId = (int)id;
-            
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                int projectId = 0;
+                projectId = (int)id;
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Project project = new Project();
+                ProjectVM projectVM = new ProjectVM();
+
+                //TODO: add error checking
+                project = (Project)Repository.GetProjectById(projectId).data;
+
+                projectVM._Id = project.Id;
+                projectVM._name = project.name;
+                projectVM._beginDate = project.beginDate;
+                projectVM._description = project.description;
+
+                if (project.status == 1)
+                {
+                    projectVM._status = true;
+                }
+                else
+                {
+                    projectVM._status = false;
+                }
+
+                if (projectVM == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(projectVM);
             }
-
-            Project project = new Project();
-            ProjectVM projectVM = new ProjectVM();
-
-            //TODO: add error checking
-            project = (Project)Repository.GetProjectById(projectId).data;
-
-            projectVM._Id = project.Id;
-            projectVM._name = project.name;
-            projectVM._beginDate = project.beginDate;
-            projectVM._description = project.description;
-
-            if (project.status == 1)
+            catch
             {
-                projectVM._status = true;
+                return View("Error");
             }
-            else
-            {
-                projectVM._status = false;
-            }
-
-            if (projectVM == null)
-            {
-                return HttpNotFound();
-            }
-            return View(projectVM);
         }
 
         // POST: Project/Edit/5
@@ -151,56 +174,70 @@ namespace HabitatForHumanity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(ProjectVM projectVM)
         {
-            if (ModelState.IsValid)
+            try
             {
-                Project project = new Project();
+                if (ModelState.IsValid)
+                {
+                    Project project = new Project();
 
-                project.Id = projectVM._Id;
-                //Checking for name
-                if (projectVM._name != null) 
-                {
-                    project.name = projectVM._name;
-                }
+                    project.Id = projectVM._Id;
+                    //Checking for name
+                    if (projectVM._name != null)
+                    {
+                        project.name = projectVM._name;
+                    }
 
-                //Checking for beginDate
-                if (projectVM._beginDate != null)
-                {
-                    project.beginDate = projectVM._beginDate;
-                }
+                    //Checking for beginDate
+                    if (projectVM._beginDate != null)
+                    {
+                        project.beginDate = projectVM._beginDate;
+                    }
 
-                //Checking for description
-                if (projectVM._description != null)
-                {
-                    project.description = projectVM._description;
+                    //Checking for description
+                    if (projectVM._description != null)
+                    {
+                        project.description = projectVM._description;
+                    }
+                    else
+                    {
+                        project.description = "";
+                    }
+
+                    //Checking for status
+                    if (projectVM._status == true)
+                    {
+                        project.status = 1;
+                    }
+                    else
+                    {
+                        project.status = 0;
+                    }
+                    db.Entry(project).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
                 }
-                else
-                {
-                    project.description = "";
-                }
-                
-                //Checking for status
-                if (projectVM._status == true)
-                {
-                    project.status = 1;
-                }
-                else
-                {
-                    project.status = 0;
-                }
-                db.Entry(project).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                return View(projectVM);
             }
-            return View(projectVM);
+            catch
+            {
+                return View("Error");
+            }
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
+            try
             {
-                db.Dispose();
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                base.Dispose(disposing);
             }
-            base.Dispose(disposing);
+            catch
+            {
+               //not sure what to put here
+            }
         }
     }
 }
