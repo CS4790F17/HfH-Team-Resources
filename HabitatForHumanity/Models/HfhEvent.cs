e4249b49b89f36e97ed598a685efd009ce8868eb
Steve Julien <stevenjulien@mail.weber.edu>
2017-12-09 16:48:50 -0700

    Events DB list, edit, delete, by name

--- HabitatForHumanity/Models/HfhEvent.cs
+++ HabitatForHumanity/Models/HfhEvent.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -35,7 +37,7 @@ namespace HabitatForHumanity.Models
 
         #region Database methods
         /// <summary>
-        /// Get all events in the database.
+        /// Get all events in the database. Ordered with most recent first
         /// </summary>
         /// <returns>A list of all events.</returns>
         public static ReturnStatus GetAllHfhEvents()
@@ -46,7 +48,7 @@ namespace HabitatForHumanity.Models
             {
                 VolunteerDbContext db = new VolunteerDbContext();
                 st.errorCode = ReturnStatus.ALL_CLEAR;
-                st.data = db.hfhEvents.ToList();
+                st.data = db.hfhEvents.OrderByDescending(e => e.startDate).ToList();
                 return st;
             }
             catch (Exception e)
@@ -86,78 +88,19 @@ namespace HabitatForHumanity.Models
             }
         }
 
-        /// <summary>
-        /// Get a single organization by name.
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns>A single organization object with a matching name otherwise null.</returns>
-        public static ReturnStatus GetOrganizationByName(string name)
-        {
-            ReturnStatus st = new ReturnStatus();
-            st.data = new Organization();
-            try
-            {
-                VolunteerDbContext db = new VolunteerDbContext();
-                st.errorCode = ReturnStatus.ALL_CLEAR;
-                st.data = db.organizations.Where(x => x.name.Equals(name)).Single();
-                return st;
-            }
-            catch (Exception e)
-            {
-                st.errorCode = ReturnStatus.COULD_NOT_CONNECT_TO_DATABASE;
-                st.errorMessage = e.ToString();
-                return st;
-            }
-        }
 
         /// <summary>
-        /// Gets all the currently active organizations
+        /// Edits the event with new values.
         /// </summary>
-        /// <returns>A list of all organizatinos that are currently active.</returns>
-        public static List<Organization> GetActiveOrganizations()
-        {
-            VolunteerDbContext db = new VolunteerDbContext();
-            return db.organizations.Where(x => x.status == 1).ToList();
-        }
-
-        /// <summary>
-        /// Adds an organization to the database.
-        /// </summary>
-        /// <param name="org">The organization to be added</param>
-        public static ReturnStatus AddOrganization(Organization org)
+        /// <param name="event">The event object with new values.</param>
+        public static ReturnStatus EditHfhEvent(HfhEvent hfhEvent)
         {
             ReturnStatus st = new ReturnStatus();
             st.data = null;
             try
             {
                 VolunteerDbContext db = new VolunteerDbContext();
-                db.organizations.Add(org);
-                db.SaveChanges();
-
-                st.errorCode = (int)ReturnStatus.ALL_CLEAR;
-                st.data = "Successfully added organization.";
-                return st;
-            }
-            catch (Exception e)
-            {
-                st.errorCode = ReturnStatus.COULD_NOT_CONNECT_TO_DATABASE;
-                st.errorMessage = e.ToString();
-                return st;
-            }
-        }
-
-        /// <summary>
-        /// Edits the organization with new values.
-        /// </summary>
-        /// <param name="org">The organization object with new values.</param>
-        public static ReturnStatus EditOrganization(Organization org)
-        {
-            ReturnStatus st = new ReturnStatus();
-            st.data = null;
-            try
-            {
-                VolunteerDbContext db = new VolunteerDbContext();
-                db.Entry(org).State = EntityState.Modified;
+                db.Entry(hfhEvent).State = EntityState.Modified;
                 db.SaveChanges();
 
                 st.errorCode = ReturnStatus.ALL_CLEAR;
@@ -172,18 +115,18 @@ namespace HabitatForHumanity.Models
         }
 
         /// <summary>
-        /// Deletes an organization from the database.
+        /// Deletes an event from the database.
         /// </summary>
-        /// <param name="org">The organization object to delete</param>
-        public static ReturnStatus DeleteOrganization(Organization org)
+        /// <param name="hfhEvent">The event object to delete</param>
+        public static ReturnStatus DeleteHfhEvent(HfhEvent hfhEvent)
         {
             ReturnStatus st = new ReturnStatus();
             st.data = null;
             try
             {
                 VolunteerDbContext db = new VolunteerDbContext();
-                db.organizations.Attach(org);
-                db.organizations.Remove(org);
+                db.hfhEvents.Attach(hfhEvent);
+                db.hfhEvents.Remove(hfhEvent);
                 db.SaveChanges();
 
                 st.errorCode = (int)ReturnStatus.ALL_CLEAR;
@@ -202,17 +145,17 @@ namespace HabitatForHumanity.Models
         /// Deletes an organization from the database by id.
         /// </summary>
         /// <param name="id">The id of the organization to delete.</param>
-        public static ReturnStatus DeleteOrganizationById(int id)
+        public static ReturnStatus DeleteHfhEventById(int id)
         {
             ReturnStatus st = new ReturnStatus();
             st.data = null;
             try
             {
                 VolunteerDbContext db = new VolunteerDbContext();
-                Organization org = db.organizations.Find(id);
-                if (org != null)
+                HfhEvent hfhEvent = db.hfhEvents.Find(id);
+                if (hfhEvent != null)
                 {
-                    db.organizations.Remove(org);
+                    db.hfhEvents.Remove(hfhEvent);
                     db.SaveChanges();
 
                     st.errorCode = ReturnStatus.ALL_CLEAR;
@@ -229,58 +172,26 @@ namespace HabitatForHumanity.Models
             }
         }
 
-        public static ReturnStatus GetOrganizationByNameSQL(string name)
+        public static ReturnStatus GetHfhEventByNameSQL(string name)
         {
             VolunteerDbContext db = new VolunteerDbContext();
             ReturnStatus st = new ReturnStatus();
-
-            var orgName = new SqlParameter("@Name", "%" + name + "%");
-
-            var orgs = db.organizations.SqlQuery("SELECT * FROM Organization WHERE Organization.name LIKE @Name", orgName).OrderByDescending(x => x.status).ToList<Organization>();
-
-
-            if (orgs.Count < 1)
+            try
             {
-                List<Organization> orgList = new List<Organization>();
-                st.data = orgList;
-                st.errorCode = ReturnStatus.ERROR_WHILE_ACCESSING_DATA;
-                return st;
-            }
+                var hfhName = new SqlParameter("@Name", "%" + name + "%");
+                var events = db.hfhEvents.SqlQuery("SELECT * FROM HfhEvent WHERE HfhEvent.name LIKE @Name", hfhName).OrderByDescending(x => x.startDate).ToList();
 
-            st.errorCode = ReturnStatus.ALL_CLEAR;
-            st.data = orgs;
+                st.data = events;
+                st.errorCode = ReturnStatus.ALL_CLEAR;
+            }
+            catch
+            {
+                st.errorCode = ReturnStatus.ERROR_WHILE_ACCESSING_DATA;
+            }
+           
             return st;
         }
 
-        public static ReturnStatus GetOrganizationSQL(string queryFilter, int status)
-        {
-            VolunteerDbContext db = new VolunteerDbContext();
-            ReturnStatus st = new ReturnStatus();
-
-
-            var orgStatus = new SqlParameter("@Status", status);
-            var orgName = new SqlParameter("@Name", "%" + queryFilter + "%");
-
-
-            var orgs = db.organizations.SqlQuery(
-                "SELECT * FROM Organization " +
-                "WHERE Organization.status = @Status " +
-                "AND Organization.name in " +
-                "(SELECT Organization.name FROM Organization WHERE Organization.name LIKE @Name)", orgStatus, orgName).OrderByDescending(x => x.status).ToList<Organization>();
-
-
-            if (orgs.Count < 1)
-            {
-                List<Organization> orgList = new List<Organization>();
-                st.data = orgList;
-                st.errorCode = ReturnStatus.ERROR_WHILE_ACCESSING_DATA;
-                return st;
-            }
-
-            st.errorCode = ReturnStatus.ALL_CLEAR;
-            st.data = orgs;
-            return st;
-        }
         #endregion
     }
 
